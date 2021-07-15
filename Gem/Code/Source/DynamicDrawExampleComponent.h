20864c6720d1bc67672bba32346c08aea6d98dd5
Tao <qingtao@amazon.com>
2021-07-15 11:41:28 -0700

    Update ASV DynamicDraw sample to validate DynamicDrawContext change for RasterPass ATOM-15935 Adding draw to pass support to DynamicDrawContext.
    Signed-off-by: Tao <qingtao@amazon.com>

--- Gem/Code/Source/DynamicDrawExampleComponent.h
+++ Gem/Code/Source/DynamicDrawExampleComponent.h
@@ -58,6 +58,10 @@ namespace AtomSampleViewer
         AZ::RHI::Ptr<AZ::RPI::DynamicDrawContext> m_dynamicDraw;
         AZ::Data::Instance<AZ::RPI::ShaderResourceGroup> m_contextSrg;
 
+        // Two dynamic draw for same pass to test sorting
+        AZ::RHI::Ptr<AZ::RPI::DynamicDrawContext> m_dynamicDraw1ForPass;
+        AZ::RHI::Ptr<AZ::RPI::DynamicDrawContext> m_dynamicDraw2ForPass;
+
         ImGuiSidebar m_imguiSidebar;
 
         bool m_showCullModeNull = true;
@@ -67,6 +71,7 @@ namespace AtomSampleViewer
         bool m_showAlphaAdditive = true;
         bool m_showLineList = true;
         bool m_showPerDrawViewport = true;
+        bool m_showSorting = true;
 
         // CommonSampleComponentBase overrides...
         void OnAllAssetsReadyActivate() override;
