699d2821f83de46c448968a33d81dda991166cd1
jromnoa <jromnoa@amazon.com>
2021-08-20 11:29:48 -0700

    convert tab to spaces inside atomsampleviewer_asset_files.cmake
    Signed-off-by: jromnoa <jromnoa@amazon.com>

--- atomsampleviewer_asset_files.cmake
+++ atomsampleviewer_asset_files.cmake
@@ -46,7 +46,7 @@ set(FILES
     Scripts/ShadowTest.bv.lua
     Scripts/StreamingImageTest.bv.lua
     Scripts/TransparentTest.bv.lua
-	Scripts/_AutomatedReviewWARPTestSuite_.bv.lua
+    Scripts/_AutomatedReviewWARPTestSuite_.bv.lua
     Scripts/_FullTestSuite_.bv.lua
     Shaders/DebugVertexNormals.azsl
     Shaders/DebugVertexNormals.materialtype
