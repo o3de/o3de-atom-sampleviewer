b2faa9f52e669d559a8839ba1e33dc02b52da1e3
Esteban Papp <81431996+amznestebanpapp@users.noreply.github.com>
2021-09-08 18:30:41 -0700

    fix for lambda unused captures (#208)
    * warning fixes
    
    Signed-off-by: Esteban Papp <81431996+amznestebanpapp@users.noreply.github.com>
    
    * another lambda unused
    
    Signed-off-by: Esteban Papp <81431996+amznestebanpapp@users.noreply.github.com>

--- Gem/Code/Source/Passes/RayTracingAmbientOcclusionPass.cpp
+++ Gem/Code/Source/Passes/RayTracingAmbientOcclusionPass.cpp
@@ -158,7 +158,6 @@ namespace AZ
 
             // Bind others for global srg
             const RHI::ShaderResourceGroupLayout* srgLayout = m_shaderResourceGroup->GetLayout();
-            
             RHI::ShaderInputBufferIndex bufferIndex;
             RHI::ShaderInputConstantIndex constantIndex;
 
