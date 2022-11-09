fcfcb4097966f84fa4ff0ba4f53b4b240bf79e92
santorac <55155825+santorac@users.noreply.github.com>
2022-11-08 21:31:31 -0800

    Removed old unused StaticMesh shader
    Signed-off-by: santorac <55155825+santorac@users.noreply.github.com>

--- atomsampleviewer_asset_files.cmake
+++ atomsampleviewer_asset_files.cmake
@@ -64,7 +64,6 @@ set(FILES
     Shaders/DebugVertexNormals.materialtype
     Shaders/DebugVertexNormals.shader
     Shaders/Instanced.azsl
-    Shaders/StaticMesh.azsl
     Shaders/ComprehensiveTestMaterial/Comprehensive.materialtype
     Shaders/ComprehensiveTestMaterial/Stacks.azsl
     Shaders/ComprehensiveTestMaterial/Stacks.shader
