e66b463d8d5992a5f044706a03e756f45fcdce55
Akio Gaule <10719597+akioCL@users.noreply.github.com>
2022-11-24 14:34:05 -0300

    VariableRateShading RHI sample
    Resolves #13388
    
    Signed-off-by: Akio Gaule <10719597+akioCL@users.noreply.github.com>

--- Gem/Code/Source/SampleComponentManager.cpp
+++ Gem/Code/Source/SampleComponentManager.cpp
@@ -64,6 +64,7 @@
 #include <RHI/BindlessPrototypeExampleComponent.h>
 #include <RHI/RayTracingExampleComponent.h>
 #include <RHI/MatrixAlignmentTestExampleComponent.h>
+#include <RHI/VariableRateShadingExampleComponent.h>
 
 #include <Performance/100KDrawable_SingleView_ExampleComponent.h>
 #include <Performance/100KDraw_10KDrawable_MultiView_ExampleComponent.h>
@@ -306,6 +307,7 @@ namespace AtomSampleViewer
             NewRHISample<TriangleExampleComponent>("Triangle"),
             NewRHISample<TrianglesConstantBufferExampleComponent>("TrianglesConstantBuffer"),
             NewRHISample<MatrixAlignmentTestExampleComponent>("MatrixAlignmentTest"),
+            NewRHISample<VariableRateShadingExampleComponent>("VariableRateShading", []() { return Utils::GetRHIDevice()->GetFeatures().m_shadingRateTypeMask != RHI::ShadingRateTypeFlags::None; }),
             NewRPISample<AssetLoadTestComponent>("AssetLoadTest"),
             NewRPISample<AuxGeomExampleComponent>("AuxGeom"),
             NewRPISample<BakedShaderVariantExampleComponent>("BakedShaderVariant"),
