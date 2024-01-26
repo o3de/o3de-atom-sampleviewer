91f7084db9063738f9307f947dc5b4b8e6712179
Tao <qingtao@amazon.com>
2024-01-25 17:08:28 -0800

    Fixed some shader issues. Fixed an assert with multi-scene sample.
    Signed-off-by: Tao <qingtao@amazon.com>

--- Gem/Code/Source/MultiSceneExampleComponent.cpp
+++ Gem/Code/Source/MultiSceneExampleComponent.cpp
@@ -172,7 +172,7 @@ namespace AtomSampleViewer
             const Vector3 nonUniformScale{ 24.f, 24.f, 1.0f };
             const Vector3 translation{ 0.f, 0.f, 0.0f };
             const auto transform = Transform::CreateTranslation(translation);
-            m_floorMeshHandle = LoadMesh(CubeModelFilePath, ModelChangedHandler{});
+            m_floorMeshHandle = LoadMesh(CubeModelFilePath, ModelChangedHandler{[](const Data::Instance<RPI::Model>&){}});
             m_meshFeatureProcessor->SetTransform(m_floorMeshHandle, transform, nonUniformScale);
         }
 
