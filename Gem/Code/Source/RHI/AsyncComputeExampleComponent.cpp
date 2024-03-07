89c45473815c6147209a1148c111bfbffc5ae6bd
Martin Winter <martin.winter@huawei.com>
2024-03-07 14:50:29 +0100

    Ray-tracing related resources
    Signed-off-by: Martin Winter <martin.winter@huawei.com>

--- Gem/Code/Source/RHI/AsyncComputeExampleComponent.cpp
+++ Gem/Code/Source/RHI/AsyncComputeExampleComponent.cpp
@@ -602,7 +602,7 @@ namespace AtomSampleViewer
         m_quadInputAssemblyBuffer = nullptr;
         m_quadStreamBufferViews.fill(AZStd::vector<AZ::RHI::SingleDeviceStreamBufferView>());
         m_terrainPipelineStates.fill(nullptr);
-        m_modelStreamBufferViews.fill(AZ::RPI::ModelLod::StreamBufferViewList());
+        m_modelStreamBufferViews.fill(AZ::RPI::ModelLod::TempStreamBufferViewList());
         m_modelPipelineStates.fill(nullptr);
         m_model = nullptr;
         m_copyTexturePipelineState = nullptr;
@@ -897,7 +897,8 @@ namespace AtomSampleViewer
                         RHI::SingleDeviceDrawItem drawItem;
                         drawItem.m_arguments = mesh.m_drawArguments;
                         drawItem.m_pipelineState = m_modelPipelineStates[ShadowScope]->GetDevicePipelineState(RHI::MultiDevice::DefaultDeviceIndex).get();
-                        drawItem.m_indexBufferView = &mesh.m_indexBufferView;
+                        auto deviceIndexBufferView{mesh.m_indexBufferView.GetDeviceIndexBufferView(RHI::MultiDevice::DefaultDeviceIndex)};
+                        drawItem.m_indexBufferView = &deviceIndexBufferView;
                         drawItem.m_shaderResourceGroupCount = static_cast<uint8_t>(RHI::ArraySize(shaderResourceGroups));
                         drawItem.m_shaderResourceGroups = shaderResourceGroups;
                         drawItem.m_streamBufferViewCount = static_cast<uint8_t>(m_modelStreamBufferViews[ShadowScope].size());
@@ -1021,7 +1022,8 @@ namespace AtomSampleViewer
                         RHI::SingleDeviceDrawItem drawItem;
                         drawItem.m_arguments = mesh.m_drawArguments;
                         drawItem.m_pipelineState = m_modelPipelineStates[ForwardScope]->GetDevicePipelineState(RHI::MultiDevice::DefaultDeviceIndex).get();
-                        drawItem.m_indexBufferView = &mesh.m_indexBufferView;
+                        auto deviceIndexBufferView{mesh.m_indexBufferView.GetDeviceIndexBufferView(RHI::MultiDevice::DefaultDeviceIndex)};
+                        drawItem.m_indexBufferView = &deviceIndexBufferView;
                         drawItem.m_shaderResourceGroupCount = static_cast<uint8_t>(RHI::ArraySize(shaderResourceGroups));
                         drawItem.m_shaderResourceGroups = shaderResourceGroups;
                         drawItem.m_streamBufferViewCount = static_cast<uint8_t>(m_modelStreamBufferViews[ForwardScope].size());
